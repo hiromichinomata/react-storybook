f5c853a0b5ba4a66d77772e186f34eb5b7f259a5
Hiromichi NOMATA <git@hiromichinomata.com>
2023-05-27 18:06:58 +0900

    add: button stories

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import './button.css';
+
+function Button({ children, color = 'default', size = 'base' }) {
+  return <button className={`${color} ${size}`}>{children}</button>;
+}
+
+export default Button;
